16ca6efb8bf907f319cc3bed2e5da80540a7e699
endenis <endenis@gmail.com>
2014-09-11 14:00:07 +0200

    #58 v2

--- 58_Function-Composition.clj
+++ 58_Function-Composition.clj
@@ -1,10 +1,11 @@
 ;; #58 Function Composition - http://www.4clojure.com/problem/58
 
-((
-
-(fn [& functions]
+(fn my-comp [& functions]
   (fn [& arguments]
-    (reduce #(vector (apply %2 %1)) arguments (reverse functions))))
+    (first
+      (reduce
+        (fn [r f]
+          (list (apply f r)))
+        arguments
+        (reverse functions)))))
 
-rest reverse) [1 2 3 4])
-; (reverse functions)
